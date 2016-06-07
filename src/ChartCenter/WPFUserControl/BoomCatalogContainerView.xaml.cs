a39a2f86ae7b4805ce5ec1899493b21c33b54572
sparkxxxxxx <5482855@qq.com>
2016-06-08 01:21:35 +0800

    load local resources

---
+++ src/ChartCenter/WPFUserControl/BoomCatalogContainerView.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChartCenter.WPFViewModel;
+using JJBoom.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using Clipboard = System.Windows.Forms.Clipboard;
+
+namespace ChartCenter.WPFUserControl
+{
+    /// <summary>
+    /// BoomCatalog.xaml 的交互逻辑
+    /// </summary>
+    public partial class BoomCatalogContainerView : UserControl
+    {
+        public BoomCatalogContainerView()
+        {
+            InitializeComponent();
+          
+        }
+ 
+
+        private void AddCatalog_OnClick(object sender, RoutedEventArgs e)
+        {
+            BoomCatalogViewModel newBoomCatalogViewModel = new BoomCatalogViewModel();
+            newBoomCatalogViewModel.BoomCatalogName = GetAvailableCatalogName("New Catalog");
+            BoomCatalogContainerViewModel boomCatalogContainerViewModel = this.DataContext as BoomCatalogContainerViewModel;
+            boomCatalogContainerViewModel.BoomCatalogViewModels.Add(newBoomCatalogViewModel);
+            MemoryStream stream = BoomWriter.SerializeToStream(BoomCatalogConvert.ConvertToBoomsCatalog(newBoomCatalogViewModel));
+            BoomWriter.StreamToFile(stream, UserInfoStorage.GetCurrentJJBoomDocumentFolderPath() + newBoomCatalogViewModel.BoomCatalogName + ".jjb");
+        }
+
+        private void DeleteCatalog_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            BoomCatalogContainerViewModel boomCatalogViewModel = menuItem.DataContext as BoomCatalogContainerViewModel;
+            boomCatalogViewModel.BoomCatalogViewModels.Remove(boomCatalogViewModel.SelectedBoomCatalogViewModel);
+        }
+
+        private void ExportCatalog_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            BoomCatalogContainerViewModel boomCatalogViewModel = menuItem.DataContext as BoomCatalogContainerViewModel;
+            boomCatalogViewModel.SelectedBoomCatalogViewModel.ExportCatalog();
+        }
+
+        private string GetAvailableCatalogName(string catalogName)
+        {
+            bool isValid = false;
+            int count = 0;
+            string newCatalogName = (string) catalogName.Clone();
+            BoomCatalogContainerViewModel boomCatalogContainerViewModel = this.DataContext as BoomCatalogContainerViewModel;
+            while (!isValid)
+            {
+                isValid = true;
+                if (count == 0)
+                {
+                    newCatalogName = string.Format("{0}", catalogName);
+                }
+                else
+                {
+                    newCatalogName = string.Format("{0}({1})", catalogName, count);
+                }
+             
+                foreach (BoomCatalogViewModel boomCatalogViewModel in boomCatalogContainerViewModel.BoomCatalogViewModels)
+                {
+                    if (boomCatalogViewModel.BoomCatalogName == newCatalogName)
+                    {
+                        count++;
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+            return newCatalogName;
+        }
+    }
+}
---
+++ src/ChartCenter/WPFUserControl/BoomCatalogView.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using ChartCenter.WPFViewModel;
+using JJBoom.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using Clipboard = System.Windows.Forms.Clipboard;
+using Image = System.Drawing.Image;
+using MenuItem = System.Windows.Controls.MenuItem;
+using Shape = System.Windows.Shapes.Shape;
+using UserControl = System.Windows.Controls.UserControl;
+using View = Microsoft.Office.Interop.PowerPoint.View;
+
+
+namespace ChartCenter.WPFUserControl
+{
+    /// <summary>
+    /// BoomCatalog.xaml 的交互逻辑
+    /// </summary>
+    public partial class BoomCatalogView : UserControl
+    {
+        
+        public BoomCatalogView()
+        {
+            InitializeComponent();
+        }
+
+        public Boom InternalBoom { get; set; }
+
+        private void MenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void ExportCatalog_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            BoomCatalogViewModel boomCatalogViewModel = menuItem.DataContext as BoomCatalogViewModel;
+            if (boomCatalogViewModel != null)
+            {
+                boomCatalogViewModel.ExportCatalog();
+            }
+        }
+
+        private void OnAddToThisCatalogClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            BoomCatalogViewModel boomCatalogViewModel = menuItem.DataContext as BoomCatalogViewModel;
+            DocumentWindow documentWindow = Globals.ThisAddIn.Application.ActiveWindow;
+            Selection selection = documentWindow.Selection;
+            try
+            {
+                if ((selection != null) && (selection.Type == PpSelectionType.ppSelectionShapes))
+                {
+                    ShapeRange shapeRange = selection.HasChildShapeRange ? selection.ChildShapeRange : selection.ShapeRange;                     
+                    shapeRange.Copy();
+                    CustomTwoTuples<string, Stream> streamAndShapeDataFormat = ClipBoardDataProvider.GetStreamFromeClipboard();
+                    Boom boom = new Boom();
+                    boom.Name = "测试";
+                    boom.Icon = Image.FromStream(ClipBoardDataProvider.GetPng());
+                    boom.ShapeData = streamAndShapeDataFormat.GetRightOne();
+                    boom.ShapeDataFormat = streamAndShapeDataFormat.GetLeftOne();
+                    BoomStencilViewModel boomStencilViewModel = new BoomStencilViewModel();
+                    boomStencilViewModel.SetCurrentViewModelByBoom(boom);
+                    if (boomCatalogViewModel != null)
+                    {
+                        boomCatalogViewModel.BoomStencilViewModels.Add(boomStencilViewModel);
+                    }
+                   
+                    MemoryStream stream = BoomWriter.SerializeToStream(BoomCatalogConvert.ConvertToBoomsCatalog(boomCatalogViewModel));
+                    BoomWriter.StreamToFile(stream,UserInfoStorage.GetCurrentJJBoomDocumentFolderPath() + boomCatalogViewModel.BoomCatalogName + ".jjb");
+                }
+            }
+            finally
+            {
+                if (selection != null)
+                {
+                    Marshal.ReleaseComObject(selection);
+                }
+            }
+        }
+
+        private string _selectedCatalogName;
+
+        private void GetSelectedCatalog(string selectedCatalogName)
+        {
+            _selectedCatalogName = selectedCatalogName;
+        }
+
+        private void OnDoubleClickBoomStencilView(object sender, MouseButtonEventArgs e)
+        {
+            ListBoxItem listBoxItem = sender as ListBoxItem;
+
+         
+            BoomStencilViewModel boomStencilViewModel = listBoxItem.Content as BoomStencilViewModel;
+
+            Clipboard.SetDataObject(StencilDataConvert.ConvertToDataObject(boomStencilViewModel.GetCurrentBoom().ShapeData, boomStencilViewModel.GetCurrentBoom().ShapeDataFormat));
+         
+            //粘贴操作
+            DocumentWindow documentWindow = Globals.ThisAddIn.Application.ActiveWindow;
+            View view = documentWindow.View;
+            view.Paste();
+        }
+    }
+}
