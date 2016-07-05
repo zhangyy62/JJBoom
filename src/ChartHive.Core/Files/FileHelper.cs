4687c2b848b9da5b2d46dca89707d5f5d1d04569
sparkxxxxxx <5482855@qq.com>
2016-07-06 00:40:10 +0800

    adjust file saved name

---
+++ src/ChartHive.Core/Files/FileHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JJBoom.Core.Files
+{
+    public static class FileHelper
+    {
+        /// <summary>
+        /// auto move file to new name path.appending '_' when same name 
+        /// </summary>
+        /// <param name="oldName"></param>
+        /// <param name="newName"></param>
+        public static void RenameFile(string oldName, string newName)
+        {
+            Directory.Move(UserInfoStorage.GetCurrentJJBoomDocumentFolderPath() + oldName + ".jjb", UserInfoStorage.GetCurrentJJBoomDocumentFolderPath() + newName + ".jjb");
+        }
+    }
+}
